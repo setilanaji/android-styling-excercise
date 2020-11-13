62bc0ed1cb950509cf4281626bcd434a150abe1c
PC-2 <setyajiyudha299@gmail.com>
2020-11-13 08:42:16 +0700

    add adapter to third activity

--- app/src/main/java/com/ydh/androiddesignstyle/ui/ThirdActivity.kt
+++ app/src/main/java/com/ydh/androiddesignstyle/ui/ThirdActivity.kt
@@ -4,18 +4,72 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.ydh.androiddesignstyle.R
+import com.ydh.androiddesignstyle.databinding.ActivityFourthBinding
+import com.ydh.androiddesignstyle.databinding.ActivityThirdBinding
+import com.ydh.androiddesignstyle.model.ProductModel
+import com.ydh.androiddesignstyle.ui.adapter.ProductAdapter
+import com.ydh.androiddesignstyle.ui.adapter.ThirdProductAdapter
+import com.ydh.androiddesignstyle.viewmodel.ProductViewModel
 
 class ThirdActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityThirdBinding
+    lateinit var productViewModel: ProductViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_third)
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setViewModel()
+        setData()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.home_menu, menu)
         return true
+    }
+
+    private fun setViewModel(){
+        productViewModel = ViewModelProviders.of(this).get(ProductViewModel::class.java)
+    }
+
+    private fun setData() {
+        productViewModel.setAllProduct()
+
+        productViewModel.data.observe({ lifecycle }, {
+            val productAdapter = ThirdProductAdapter(this, it as MutableList<ProductModel>,
+                object : ThirdProductAdapter.PostItemListener {
+                    override fun onPostClick(productModel: ProductModel) {
+//                        val intent = Intent(this@FourthActivity, ::class.java)
+//                        intent.putExtra("id", userModel.id)
+//                        intent.putExtra("firstName", userModel.firstName)
+//                        intent.putExtra("lastName", userModel.lastName)
+//                        intent.putExtra("email", userModel.email)
+//                        intent.putExtra("url", userModel.avatarImgUrl)
+//                        startActivity(intent)
+                    }
+
+                })
+            val recyclerView = binding.recyclerThirdProduct
+
+            recyclerView.apply {
+                this.layoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
+                this.adapter = productAdapter
+            }
+        })
+
+        productViewModel.response.observe({ lifecycle }, {
+            if (it.isNotEmpty()) {
+                Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 
 }
---
+++ app/src/main/java/com/ydh/androiddesignstyle/ui/adapter/ThirdProductAdapter.kt
@@ -0,0 +1,67 @@
+package com.ydh.androiddesignstyle.ui.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.Target
+import com.ydh.androiddesignstyle.R
+import com.ydh.androiddesignstyle.model.ProductModel
+
+class ThirdProductAdapter (private val context: Context, private val items: List<ProductModel>, private val  itemListener: PostItemListener): RecyclerView.Adapter<ThirdProductAdapter.ProductViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+
+        val inflater = LayoutInflater.from(context)
+        val view: View = inflater.inflate(R.layout.third_item, parent, false)
+        return ProductViewHolder(view, this.itemListener)
+    }
+
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+        val product = items[position]
+        holder.title?.text = product.title
+        holder.price?.text = product.category
+        holder.image?.let { Glide.with(context).load(product.imageUrl).override(
+            Target.SIZE_ORIGINAL,
+            Target.SIZE_ORIGINAL
+        ).into(it) }
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    fun getItem(position: Int): ProductModel {
+        return items[position]
+    }
+
+    interface PostItemListener {
+        fun onPostClick(postModel: ProductModel)
+    }
+
+    inner class ProductViewHolder(var view: View, var itemListener: PostItemListener) :
+        RecyclerView.ViewHolder(view), View.OnClickListener {
+        var title: TextView? = null
+        var price: TextView? = null
+        var image: ImageView? = null
+
+        init {
+            title = view.findViewById(R.id.tv_third_item_title)
+            price = view.findViewById(R.id.tv_third_item_price)
+            image = view.findViewById(R.id.image_third_item)
+            view.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            val product = getItem(adapterPosition)
+            this.itemListener.onPostClick(product)
+            this@ThirdProductAdapter.notifyDataSetChanged()
+
+        }
+
+    }
+
+}
